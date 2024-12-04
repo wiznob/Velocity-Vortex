44653b1a5aee602d0e78d4d4d6391c2ef6efa045
Your Name <93250497+wiznob@users.noreply.github.com>
2024-12-04 19:30:53 +0000

    changed speed timers to sprint button

--- Player/dash.gd
+++ Player/dash.gd
@@ -6,7 +6,7 @@ extends CharacterBody3D
 
 @export_group("Movement")
 @export var move_speed = 8.0
-@export var boost_speed = 11.0
+@export var boost_speed = 16.0
 @export var acceleration := 20.0
 @export var rotation_speed := 12
 @export var jump_impulse := 12.0
@@ -22,8 +22,6 @@ var _gravity := -30.0
 
 @onready var _camera_pivot: Node3D = %CameraPivot
 @onready var _camera: Camera3D = %Camera3D
-@onready var _boost_timer = %BoostTimer
-@onready var _normal_timer = %NormalTimer
 
 
 func _input(event: InputEvent) -> void:
@@ -55,31 +53,33 @@ func _physics_process(delta: float) -> void:
 	_camera_input_direction = Vector2.ZERO
 	
 	# movement logic
+	var sprint := Input.is_action_pressed("run")
 	var raw_input := Input.get_vector("move_left", "move_right","move_up","move_down")
 	var forward := _camera.global_basis.z
 	var right := _camera.global_basis.x
 	var move_direction := forward * raw_input.y + right * raw_input.x
 	move_direction.y = 0.0
 	move_direction = move_direction.normalized()
-	
-	#speed mode (defnitley need improvement lol)
-	if velocity.length() >= 7.5 and _boost_timer.is_stopped() == true and move_speed == 8:
-		_boost_timer.start()
-	elif velocity.length() <= 3.0 and _normal_timer.is_stopped() and move_speed > 8:
-		_normal_timer.start()
+	#yeahhh a sprint button works much better.....
+	if sprint == true:
+		move_speed = boost_speed
+	else:
+		move_speed = 8.0
 
 	var y_velocity := velocity.y
 	velocity.y = 0.0
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
 	velocity.y = y_velocity + _gravity * delta
-	print(velocity)
 	
 	#Jump logic
 	var is_starting_jump := Input.is_action_just_pressed("jump") and (is_on_floor() or current_jumps < max_jumps)
-	if Input.is_action_just_pressed("jump") and is_on_wall() and not is_on_floor():#wall jump
+	if Input.is_action_just_pressed("jump") and is_on_wall_only():#wall jump
 		# Apply bounce in the opposite direction
+		var current_z_velocity = velocity.z# only works if wall wall normal is on z axis 
 		velocity = get_wall_normal() * wall_jump_impulse
 		velocity.y = jump_impulse
+		velocity.z = current_z_velocity
+		#velocity.x = current_x_velocity
 		#sticky jump?
 		# wall_friction *= delta
 	elif is_on_wall_only():#Wall run
@@ -90,15 +90,8 @@ func _physics_process(delta: float) -> void:
 		current_jumps += 1
 	elif is_on_floor():
 		current_jumps = 0
-	
 	move_and_slide()
 
 	if move_direction.length() > 0.2:
 		_last_movement_direction = move_direction
 	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
-
-func _on_boost_timer_timeout():
-	move_speed = boost_speed
-	
-func _on_normal_timer_timeout():
-	move_speed = 8.0
